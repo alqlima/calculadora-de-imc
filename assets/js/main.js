8b18757f947e7bc525fbe240c144e4559c0dbc07
André Queiroz <alqlima@gmail.com>
2020-10-09 11:44:37 -0300

    Capture the form submit event

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// Capture the form submit event
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log('Evento previnido');
+    setResultado('Olá Mundo!!!');
+});
+
+function criaP() {
+    const p = document.createElement('p');
+    return p;
+};
+
+function setResultado(msg) {
+    const resultado = document.querySelector('#resultado');
+    resultado.innerHTML = '';
+    const p = criaP();
+};
+
+
